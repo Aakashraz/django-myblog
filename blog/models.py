71f4335b885ec311f7ae72081a693826240e40bd
Aakashraz <aakashraz@outlook.com>
2024-10-27 23:34:34 +0545

    changes made to administration page

--- blog/models.py
+++ blog/models.py
@@ -14,11 +14,14 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
+    # This field defines a many-to-one relationship with the default user model,
+    # meaning that each post is written by a user, and a user can write any number of posts.
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+    # need to understand the below code
     status = models.CharField(
         max_length=2,
         choices=Status,
@@ -27,6 +30,7 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-publish']
+        # also this?
         indexes = [
             models.Index(fields=['-publish']),
         ]
