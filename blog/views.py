72613ae883bd737932a9bec2672dc5ee1e3b8f7b
Aakashraz <aakashraz@outlook.com>
2024-11-25 21:44:24 +0545

    pagination error handling of EmptyPage and PageNotAnInteger

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from .models import Post
 from django.http import Http404
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def post_list(request):
@@ -11,10 +11,19 @@ def post_list(request):
     # We retrieve the page GET HTTP parameter and store it in the page_number variable.
     # This parameter contains the requested page number. If the page parameter is not in the GET parameters
     # of the request, we use the default value 1 to load the first page of results.
-
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
     # We obtain the objects for the desired page by calling the page() method of Paginator. This
     # method returns a Page object that we store in the posts variable.
+    except (EmptyPage, PageNotAnInteger) as e:
+        # if page_number is not an integer get the first page
+        if isinstance(e, PageNotAnInteger):
+            posts = paginator.page(1)
+
+        # if page_number is out of range get last page or results
+        else:
+            # paginator.num_pages will return total no. os pages, which is also last page no.
+            posts = paginator.page(paginator.num_pages)
 
     return render(
         request,
