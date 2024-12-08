96abf0c2c7814bf00ba22bd373f2a84ab678f432
Aakashraz <aakashraz@outlook.com>
2024-12-08 23:30:19 +0545

    tag added in the post_list view and url pattern

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,12 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    # path('', views.post_list, name='post_list'),
-    path('', views.PostListView.as_view(), name='post_list'),
+    # Post views
+    path('', views.post_list, name='post_list'),
+    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
+    # both above patterns points to the same view, but they have different names.
+    # path('', views.PostListView.as_view(), name='post_list'),
+
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail, name='post_detail'),
     path('redirect/', views.redirect_me, name='redirect_me'),
